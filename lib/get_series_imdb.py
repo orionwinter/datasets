97f77bfc36a13cdc58acf29299c0d2b0cf6630f7
Órion Winter <orion.winter66@gmail.com>
2019-07-10 16:38:19 -0300

    Adiciona 5 séries

--- lib/get_series_imdb.py
+++ lib/get_series_imdb.py
@@ -6,11 +6,11 @@ import numpy as np
 series_imdb = pd.read_csv("https://raw.githubusercontent.com/nazareno/imdb-series/master/data/series_from_imdb.csv")
 
 got_e_bb = series_imdb.query(
-    "series_name == 'Breaking Bad' | series_name == 'Game of Thrones'"
+    "series_name in ['Breaking Bad', 'Game of Thrones', 'Vikings', 'Better Call Saul', 'Homeland']"
 ).reset_index(
     drop=True
 ).filter(
     items=["series_name", "episode", "series_ep", "season", "season_ep", "user_rating", "user_votes"]        
 )
 
-got_e_bb.to_csv("../docs/series_imdb.csv", index=False)
+got_e_bb.to_csv("../docs/series_imdb_5_series.csv", index=False)
